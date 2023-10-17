da19638c7dadf0aba8d360a2bf8c55951e88dc1a
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-10-17 09:35:50 +0000

    string

---
+++ trymyapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import String from './components/String';
+
+function App() {
+  return (
+    <div className="App">
+     <String/>
+    </div>
+  );
+}
+
+export default App;
---
+++ trymyapp/src/components/String.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function String() {
+  return (
+    <>
+        <h1>Inpu Words</h1>
+        <input></input>
+        
+    </>
+  )
+}
+
+export default String
